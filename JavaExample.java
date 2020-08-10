f036e1d7834743345920747d2e9fb19f8bfb69b0
pagb <paolag010@gmail.com>
2020-08-10 17:15:56 +0200

    Update JavaExample.java

--- JavaExample.java
+++ JavaExample.java
@@ -2,10 +2,16 @@ public class JavaExample{
 
   public static void main(String[] args){
     System.out.println("Test");
+    System.out.println(getValue());
+    System.out.println(getValue("Eric"));
   }
   
   public static String getValue() {
-    return "test02";
+    return "test";
+  }
+  
+  public static String getValue(String name) {
+    return name + "test";
   }
 
 }
