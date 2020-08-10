5cc4fbea2fc262f862e4d29083b22e34bb41038f
pagb <paolag010@gmail.com>
2020-08-10 17:26:47 +0200

    Update JavaExample.java

--- JavaExample.java
+++ JavaExample.java
@@ -3,7 +3,7 @@ public class JavaExample{
   public static void main(String[] args){
     System.out.println("Test");
     System.out.println(getValue());
-    System.out.println(getValue("Eric 1"));
+    System.out.println(getValue("Eric 2"));
   }
   
   public static String getValue() {
